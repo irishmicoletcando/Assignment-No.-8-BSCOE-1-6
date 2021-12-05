2968cfc833c800588693815c197f6b08e050f66a
Irish Micole Cando <irishmicolecando@gmail.com>
2021-12-05 20:38:21 +0800

    Added while loop in if statement and a variable for user to input the guess

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -18,7 +18,8 @@ def guessing_number():
     ask_user_ready_or_not = input("Ready? Type y if yes and n if not. ").lower()
 
     if ask_user_ready_or_not == "y":
-    
+        while True:
+            user_number = int(input("Your guess: "))
 
     else:
         print("Please restart the game.")
